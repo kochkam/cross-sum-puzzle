adceed137b190b816abf6ac1eb8f9998a9fd2cd5
Michael <mjkochka@gmail.com>
2021-03-03 19:18:55 -0800

    updates

--- cross-sum-puzzle/src/App.js
+++ cross-sum-puzzle/src/App.js
@@ -8,12 +8,124 @@ import checkerArray from './array';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
 
 
 
 
 class Board extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    valid : "Neutral"
+    }
+  }
+ 
+  checkColumn(colSum,id){
+    var validAns = ["1","2","3","4","5","6","7","8","9"];
+    var ans = parseInt(colSum)
+    var total = 0;
+    var newindex = parseInt(id.id) + 8
+    if (newindex > 72) {
+      return true
+    }
+    id = document.getElementById(newindex.toString())
+    if (id.className == "darksquare custom-select") {
+      return true; 
+    }
 
+    while (id.className != "darksquare custom-select" && newindex < 73) {
+      if (validAns.includes(id.value)) {
+      validAns.splice(validAns.indexOf(id.value),1)
+      total = total + parseInt(id.value)
+      newindex = parseInt(id.id) + 8
+      if (newindex < 73) {
+        id = document.getElementById(newindex.toString())}
+      }
+      else return false; 
+    }
+    if (total.toString() != colSum) {
+      return false
+    }
+    return true
+  }
+
+  checkRow(rowSum, id) {
+    var validAns = ["1","2","3","4","5","6","7","8","9"];
+    var ans = parseInt(rowSum)
+    var total = 0;
+    var newindex = parseInt(id.id) + 1
+    if (newindex > 72) {
+      return true
+    }
+    id = document.getElementById(newindex.toString())
+    if (id.className == "darksquare custom-select") {
+      return true; 
+    }
+
+    while (id.className != "darksquare custom-select" && newindex < 73) {
+      if (validAns.includes(id.value)) {
+      validAns.splice(validAns.indexOf(id.value),1)
+      total = total + parseInt(id.value)
+      newindex = parseInt(id.id) + 1
+      if (newindex < 73) {
+        id = document.getElementById(newindex.toString())}
+      }
+      else return false; 
+    }
+    if (total.toString() != rowSum) {
+      return false
+    }
+    return true
+
+
+  }
+
+
+checkSolution(){
+
+  for (let index = 1; index < 73; index++) {
+    var id = document.getElementById(index.toString())
+    var correct = true; 
+    var altcorrect = true;
+    if (id.className === "darksquare custom-select" && id.value != "") {
+      var answer = id.value;
+      var one = answer.split("\\")
+      if (one[0] == "") {
+          correct = this.checkRow(one[1],id)
+        }
+     
+      else if(one[1] === "" && one[0] !== ""){
+        correct = this.checkColumn(one[0],id);
+      }
+      else{
+        correct = this.checkColumn(one[0],id);
+        altcorrect = this.checkRow(one[1],id)
+      }
+      if (correct === false || altcorrect === false) {
+        return false
+      }
+    }
+    
+  }
+  return true;
+
+}
+
+
+
+validate(event){
+  console.log("working")
+  event.preventDefault(); 
+  if (this.checkSolution()) {
+    this.setState({valid: "True"})
+    
+  }
+  else {
+    this.setState({valid: "False"})}
+
+
+}
 
   renderBlankSquare(i, title = ""){
     return(
@@ -46,6 +158,7 @@ class Board extends React.Component{
     let counter = 1;
     let rows = [];
     var cells = []
+    var status; 
 
     cells.push(this.renderBlankSquare(counter))
     counter = counter + 1; 
@@ -208,8 +321,14 @@ class Board extends React.Component{
     cells.push(this.renderSquareAlt(counter))
     counter = counter + 1;
     rows.push(<Form.Row className='row'>{cells}</Form.Row>)
-
-
+    if (this.state.valid == "False") {
+      status = "Invalid Solution"
+    }
+    else if(this.state.valid == "True") { 
+      status = "Correct"}
+    else {
+      status = ""
+    }
 
 
     
@@ -221,9 +340,14 @@ class Board extends React.Component{
 
 
     return(
-      <Form >
+      <div>
+      <Form key="main"onSubmit={this.validate.bind(this)} >
       {rows}
+      <Button type="submit" variant="primary">Check Solution</Button>
       </Form>
+      <div>{status}</div>
+      </div>
+    
 
 
 
